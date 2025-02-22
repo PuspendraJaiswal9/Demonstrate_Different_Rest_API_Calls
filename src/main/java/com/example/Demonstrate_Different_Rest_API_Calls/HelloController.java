a2d0a8d370eb7909d96804ede47c3217393a3096
puspendraJaiswal9 <pushpendrajaiswal988@gmail.com>
2025-02-22 11:12:39 +0530

    [Puspendra]Add. show Hello Mark from BridgeLabz

--- src/main/java/com/example/Demonstrate_Different_Rest_API_Calls/HelloController.java
+++ src/main/java/com/example/Demonstrate_Different_Rest_API_Calls/HelloController.java
@@ -2,6 +2,7 @@ package com.example.Demonstrate_Different_Rest_API_Calls;
 
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
@@ -10,5 +11,11 @@ public class HelloController {
     @GetMapping("/hi")
     public String sayHello() {
         return "Hello from BridgeLabz";
+    }
+
+
+    @GetMapping("/h")
+    public String sayHelloMark(@RequestParam String name){
+        return "Hello "+name+" from BridgeLabz";
     }
 }
